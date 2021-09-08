ee6e0d18db3da745f4fa1253e6d28cb303ab7558
GuilhermeFG <guilhermefguimaraes23@gmail.com>
2021-09-08 12:07:39 -0300

    deu certoo

--- reactjs/src/pages/login/index.js
+++ reactjs/src/pages/login/index.js
@@ -1,11 +1,44 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import LoadingBar from 'react-top-loading-bar';
+
+
 import { Container } from './styled'
 import { ChatButton, ChatInput } from '../../components/outros/inputs'
+import { useState, useRef } from 'react'
 
+import Cookies from 'js-cookie';
+import Api from '../../service/api';
+import { useHistory } from 'react-router-dom'
+
+const api = new Api(); 
 
 export default function Login() {
+    const [login, setLogin] = useState('');
+    const [senha, setSenha] = useState('');
+
+
+    const navig = useHistory();
+    const loading = useRef(null);   
+
+    const logar = async () => {
+        loading.current.continuousStart();
+        
+
+        let resp = await api.login(login, senha);
+        if (resp.erro) {
+            toast.error(`${resp.erro}`)
+            loading.current.complete();
+        } else {
+            Cookies.set('usuario-logado', JSON.stringify(resp));
+            navig.push('/chat')
+        }
+    }
 
     return (
         <Container>
+             <ToastContainer />
+            <LoadingBar color="red" ref={loading} />
             <div className="box">
                 <div className="titulo">
                     <img src="/assets/images/logo-monkchat.png" alt="" />
@@ -24,18 +57,23 @@ export default function Login() {
                         <div>
                             <div className="label">Login </div>
                             <ChatInput
+                                value={login}
+                                onChange={e => setLogin(e.target.value)}
                                 style={{ border: '1px solid gray', fontSize: '1.5em' }}
                                 />
                         </div>
                         <div>
                             <div className="label">Senha </div>
                             <ChatInput
+                                value={senha}
+                                onChange={e => setSenha(e.target.value)}
                                 type="password"
                                 style={{ border: '1px solid gray', fontSize: '1.5em' }}
                                 />
                         </div>
                         <div>
                             <ChatButton
+                                onClick={logar}
                                 style={{ fontSize: '1.2em'}}> Login </ChatButton>
                         </div>
                     </div>
